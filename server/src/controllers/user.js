962a42e780dbab145f2b017c019e05f6d8b2b7ac
unknown <xuantho573@gmail.com>
2022-08-27 19:11:12 +0700

    Initialize middleware for getting user and export it

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,1 +1,15 @@
 import User from "../models";
+
+// [GET] get user homepage (with todo list)
+async function get(req, res) {
+    console.log(`---------- Getting user ----------\n`);
+    try {
+        const user = await User.findById(req.params._id);
+        res.json(user);
+        console.log(`---------- User found ----------\n`);
+    } catch (err) {
+        console.log(`---------- Failed to get user ----------\n`);
+    }
+}
+
+export { get };
