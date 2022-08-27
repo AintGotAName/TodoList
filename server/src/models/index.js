0db7866ab4abbdc82f1727048d631a5ba53140d1
unknown <xuantho573@gmail.com>
2022-08-27 20:05:30 +0700

    Remove versionKey field

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,18 +1,21 @@
 import { Schema, model, Types } from "mongoose";
 
-const UserSchema = new Schema({
-    username: String,
-    hash: String,
-    salt: String,
-    todoList: [
-        {
-            _id: Types.ObjectId,
-            createdAt: Date,
-            finishedAt: Date,
-            content: String,
-            description: String,
-        },
-    ],
-});
+const UserSchema = new Schema(
+    {
+        username: String,
+        hash: String,
+        salt: String,
+        todoList: [
+            {
+                _id: Types.ObjectId,
+                createdAt: Date,
+                finishedAt: Date,
+                content: String,
+                description: String,
+            },
+        ],
+    },
+    { versionKey: false }
+);
 
 export default model("User", UserSchema);
